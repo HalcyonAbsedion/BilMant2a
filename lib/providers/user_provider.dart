1c5fb54fe64d834f1894e3d4d5d1f1d7da8875f6
Ali Sarawan <ali.sarawan13@gmail.com>
2024-03-23 15:12:15 +0200

    finished user_provider

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -1,11 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+import 'package:bilmant2a/models/user.dart';
+import 'package:bilmant2a/services/auth_service.dart';
+import 'package:flutter/widgets.dart';
 
 class UserProvider with ChangeNotifier {
   User? _user;
+  final AuthMethods _authMethods = AuthMethods();
 
   User get getUser => _user!;
 
-  Future<void> refreshUser() async {}
+  Future<void> refreshUser() async {
+    User user = await _authMethods.getUserDetails();
+    _user = user;
+    notifyListeners();
+  }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-import 'package:bilmant2a/services/storage_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:bilmant2a/models/user.dart' as model;
