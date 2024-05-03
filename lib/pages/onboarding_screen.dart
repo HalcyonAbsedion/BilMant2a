58cd8dc38b31a61bbda4589b2f972a5f81cfff44
IAI236 <iai077@student.bau.edu.lb>
2024-05-03 19:56:04 +0300

    added intro screens (needs functionality)

---
+++ lib/pages/onboarding_screen.dart
@@ -0,0 +1,85 @@
+import 'package:bilmant2a/pages/home_page.dart';
+import 'package:bilmant2a/pages/intro_screens/intro_page1.dart';
+import 'package:bilmant2a/pages/intro_screens/intro_page2.dart';
+import 'package:bilmant2a/pages/intro_screens/intro_page3.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  _OnBoardingScreenState createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  PageController _controller = PageController();
+
+  // keep track of if we are on the last page or not
+  bool onLastPage = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
+            children: const [
+              IntroPage1(),
+              IntroPage2(),
+              IntroPage3(),
+            ],
+          ),
+
+          // dot indicators
+          Container(
+              alignment: Alignment(0, 0.8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  //skip button
+                  GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                    },
+                    child: Text('Skip'),
+                  ),
+                  // dots
+                  SmoothPageIndicator(controller: _controller, count: 3),
+
+                  //next or done button
+                  onLastPage
+                      ? GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return HomePage();
+                                },
+                              ),
+                            );
+                          },
+                          child: Text('Done'),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            //navigate to the next screen
+                          },
+                          child: Text('Next'),
+                        )
+                ],
+              )),
+        ],
+      ),
+    );
+  }
+}
